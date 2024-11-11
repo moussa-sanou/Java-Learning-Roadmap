48599cc5395d64f869d2ed989a98fc29fa563670
samidollars <muss.junior@gmail.com>
2024-11-10 21:06:59 -0600

    Day 1

---
+++ Week 1/src/Main.java
@@ -0,0 +1,10 @@
+/*Week 1:
+* Day 1: Install JDK, choose IDE (IntelliJ, Eclipse), write and run a simple "Hello world"*/
+public class Main {
+    public static void main(String[] args) {
+
+        System.out.println("Hello and welcome!");
+        System.out.println("This is a new beginning");
+
+    }
+}
