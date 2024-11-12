683fe9a839c6289d3d9627174a44928461369f3e
samidollars <muss.junior@gmail.com>
2024-11-11 22:44:38 -0600

    Day 1: Install JDK and IDE write and run a simple "Hello World"

--- Week 1/src/day1.java
+++ Week 1/src/day1.java
@@ -1,6 +1,16 @@
 public class day1 {
+
     public static void main(String[] args) {
         //This is your first code "Hello World"
         System.out.println("Hello World");
     }
+
+    /* What is java?
+    * Java is a very commonly used programming language
+    *
+    * What is a programming language?
+    * A programming language contains a set of defined vocabulary.
+    * A programming language contains a set of grammatical rules.
+    * A programming language allow us to communicate with computing devices to complete
+    * certain tasks*/
 }
