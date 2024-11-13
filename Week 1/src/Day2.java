5ef3b777eb351102da38111021aa7b8bb6af0ec1
samidollars <muss.junior@gmail.com>
2024-11-12 22:56:43 -0600

    Day 2: Variables and data types

---
+++ Week 1/src/Day2.java
@@ -0,0 +1,21 @@
+public class Day2 {
+
+    public static void main(String[] args) {
+        int studentAge = 15;
+        double studentGPA = 3.45;
+        char studentFirstInitial = 'S';
+        char studentLastInitial = 'D';
+        boolean hasPerfectAttendance = true;
+        String studentFirstName = "Sami";
+        String studentLastName = "Dollars";
+
+        System.out.println(studentAge);
+        System.out.println(studentGPA);
+        System.out.println(studentFirstInitial);
+        System.out.println(studentLastInitial);
+        System.out.println(hasPerfectAttendance);
+        System.out.println(studentFirstName);
+        System.out.println(studentLastName);
+
+    }
+}
