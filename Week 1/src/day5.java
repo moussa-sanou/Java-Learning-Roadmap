1904e0b668039c0f4082e6277de7f4aaaccef0c8
samidollars <muss.junior@gmail.com>
2024-11-16 23:10:47 -0600

    Day 5: while loop

---
+++ Week 1/src/day5.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class day5 {
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        boolean isOnRepeat = true;
+        while (isOnRepeat){
+            System.out.println("Playing current song");
+            System.out.println("Would you like to take this song off repeat? If so, answer yes");
+            String userInput = input.next();
+
+            if (userInput.equals("yes")){
+                isOnRepeat = false;
+            }
+        }
+        System.out.println("Playing next song");
+    }
+}
