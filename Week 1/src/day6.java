ad56578cf5a9b6c0e77500baa293b77e71ce40c9
samidollars <muss.junior@gmail.com>
2024-11-18 23:19:16 -0600

    Day 6: Function in Java

---
+++ Week 1/src/day6.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class day6 {
+
+    public static void announceDeveloperTeaTime(){
+        System.out.println("Waiting for developer tea time...");
+        System.out.println("Type in a random word and press enter to start developer time");
+        Scanner input = new Scanner(System.in);
+        input.next();
+        System.out.println("It's developer tea time!");
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Welcome to your new job");
+
+        announceDeveloperTeaTime();
+
+        System.out.println("Write code");
+        System.out.println("Review code");
+
+        announceDeveloperTeaTime();
+
+        System.out.println("Get promoted");
+
+    }
+}
