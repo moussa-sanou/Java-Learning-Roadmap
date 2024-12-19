f79197a3c31dc3dced65a6f342be5efceb7e7003
samidollars <muss.junior@gmail.com>
2024-12-18 23:22:21 -0600

    Day 2: Demonstrate basic structure of a java application

---
+++ JavaFoundation/src/week_1/Lincoln_day2.java
@@ -0,0 +1,14 @@
+package week_1;
+
+// Demonstrate the basic structure of a Java application
+
+public class Lincoln_day2 {
+
+    // Prints a presidential quote
+    public static void main(String[] args) {
+
+        System.out.println("A quote by Abraham Lincoln:");
+
+        System.out.println("Whatever you are, be a good one. ");
+    }
+}
