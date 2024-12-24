9aec1d41845c3a0356c809c2f1f86a0cf711d24d
samidollars <muss.junior@gmail.com>
2024-12-23 23:18:46 -0600

    Day 6: Demonstrates the use of the nextline method of the scanner class to read a string from the user

---
+++ JavaFoundation/src/week_1/Day6_Echo.java
@@ -0,0 +1,22 @@
+package week_1;
+
+/* Demonstrates the use of the nextLine method of the Scanner class to read a string from the user. */
+
+import java.util.Scanner;
+
+public class Day6_Echo {
+
+    // Reads a character string from the user and prints it.
+
+    public static void main(String[] args) {
+
+        String message;
+        Scanner scan = new Scanner(System.in);
+
+        System.out.println("Enter a line of text: ");
+
+        message = scan.nextLine();
+        System.out.println("You entered: " + "\n" + message + "\n");
+
+    }
+}
