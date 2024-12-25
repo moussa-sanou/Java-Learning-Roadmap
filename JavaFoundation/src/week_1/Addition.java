46a1ba0a9e42d131402a8431840f466547ed4a8a
samidollars <muss.junior@gmail.com>
2024-12-24 23:08:52 -0600

    Day 7: Data and Expressions

---
+++ JavaFoundation/src/week_1/Addition.java
@@ -0,0 +1,13 @@
+package week_1;
+
+/* Demonstrates the difference the addition and string concatenation operators. */
+
+public class Addition {
+
+    // Concatenates and adds two numbers and prints results
+    public static void main(String[] args) {
+        System.out.println("24 and 45 concatenated: " + 24 + 45);
+
+        System.out.println("24 and 45 added: " + (24 + 45));
+    }
+}
---
+++ JavaFoundation/src/week_1/Day7_GasMileage.java
@@ -0,0 +1,26 @@
+package week_1;
+
+// Demonstrates the use of the scanner class to read numeric data
+
+import java.util.Scanner;
+
+public class Day7_GasMileage {
+
+    /* Calculates fuel efficiency based on value entered by the user */
+    public static void main(String[] args) {
+
+        int miles;
+        double gallons, mpg;
+        Scanner scan = new Scanner(System.in);
+
+        System.out.println(" Enter the number of miles: ");
+        miles = scan.nextInt();
+
+        System.out.println(" Enter the gallons of fuel used: ");
+        gallons = scan.nextDouble();
+
+        mpg = miles / gallons;
+
+        System.out.println(" Miles Per Gallon: " + mpg);
+    }
+}
