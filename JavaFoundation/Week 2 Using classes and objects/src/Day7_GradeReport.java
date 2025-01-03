b4398d20a3add53804c2e4b277b50814286f105a
samidollars <muss.junior@gmail.com>
2025-01-02 22:43:01 -0600

    Week 2: Day7: Demonstrate the use of a switch statement.

---
+++ JavaFoundation/Week 2 Using classes and objects/src/Day7_GradeReport.java
@@ -0,0 +1,43 @@
+// Demonstrate the use of a switch statement
+
+import java.util.Scanner;
+
+public class Day7_GradeReport {
+
+    /* Reads a grade from the user and prints comments accordingly */
+    public static void main(String[] args) {
+
+        int grade, category;
+
+        Scanner scan = new Scanner(System.in);
+
+        System.out.println("Enter a numeric grade (0 to 100): ");
+        grade = scan.nextInt();
+
+        category = grade / 10;
+
+        System.out.println("That grade is ");
+
+        switch (category)
+        {
+            case 10:
+                System.out.println("a perfect score. Well done.");
+                break;
+            case 9:
+                System.out.println("well above average. Excellent.");
+                break;
+            case 8:
+                System.out.println("above average. Nice job.");
+                break;
+            case 7:
+                System.out.println("average");
+                break;
+            case 6:
+                System.out.println("below average. Please see the ");
+                System.out.println("instructor for assistance.");
+                break;
+            default:
+                System.out.println("not passing.");
+        }
+    }
+}
