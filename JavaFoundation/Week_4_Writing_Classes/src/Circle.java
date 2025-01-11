2f6417c68007e5ba93183315f81306377e108fd3
samidollars <muss.junior@gmail.com>
2025-01-10 22:33:47 -0600

    Week 4: Create and call a Constructor

---
+++ JavaFoundation/Week_4_Writing_Classes/src/Circle.java
@@ -0,0 +1,27 @@
+/* ...CONSTRUCTOR...*/
+// A constructor is a special method which can be used to initialize an object data
+
+public class Circle {
+
+    double radius;
+    Circle(double r){
+        radius = r;
+        System.out.println("Circle (" + radius +")");
+    }
+    Circle(){
+        radius = 5.0;
+        System.out.println("Circle (" + radius + ")");
+    }
+    double findArea(){
+        return radius * radius * Math.PI;
+    }
+    double findCircumference(){
+        return 2 * Math.PI * radius;
+    }
+
+    public static void main(String[] args) {
+        Circle circle = new Circle();
+
+    }
+
+}
