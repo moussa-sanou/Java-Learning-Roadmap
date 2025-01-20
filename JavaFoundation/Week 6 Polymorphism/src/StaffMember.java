14120b9eac5c8cd0a827a87323ff996e2aabacf5
samidollars <muss.junior@gmail.com>
2025-01-19 22:38:40 -0600

    Week 6: Polymorphism Represents the personnel staff of a particular business.

--- JavaFoundation/Week 6 Polymorphism/src/StaffMember.java
+++ JavaFoundation/Week 6 Polymorphism/src/StaffMember.java
@@ -1,2 +1,8 @@
-public class StaffMember {
+// Represents a generic staff member
+
+abstract public class StaffMember {
+
+    protected String name;
+    protected String address;
+    protected String phone;
 }
