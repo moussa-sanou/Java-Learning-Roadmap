7a713da928ac39bfa773cc68199174f859d6734c
samidollars <muss.junior@gmail.com>
2025-01-20 23:30:36 -0600

    Week 6: Polymorphism Demonstrates polymorphism via inheritance.

---
+++ JavaFoundation/Week 6 Polymorphism/src/Firm.java
@@ -0,0 +1,10 @@
+// Demonstrates polymorphism via inheritance
+
+public class Firm {
+
+    // Creates a staff of employee for firm and pays them.
+    public static void main(String[] args) {
+        Staff personnel = new Staff();
+        personnel.payday();
+    }
+}
