696932536fc213e5279f2087d6f93f1157ef5bee
samidollars <muss.junior@gmail.com>
2025-01-22 23:24:29 -0600

    Week 7: Exception

---
+++ JavaFoundation/Week 7 Exception/src/Test.java
@@ -0,0 +1,10 @@
+// Write a method with an exception
+
+public class Test {
+    public static void main(String[] args) {
+        String s = null;
+        if (s.equals("Hello")){
+            System.out.println("equal");
+        }
+    }
+}
