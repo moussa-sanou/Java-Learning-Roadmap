66eb009ac4d75689e9b3acf996e2713a19e87c92
samidollars <muss.junior@gmail.com>
2025-02-11 22:51:47 -0600

    Week 11: Inheritance

---
+++ JavaFoundation/Week 11 Inheritance Continued/src/Dictionary.java
@@ -0,0 +1,25 @@
+/* Represents a dictionary, which is a book. Used to demonstrate
+* inheritance. */
+
+public class Dictionary extends Book {
+
+    private int definitions = 52500;
+
+    // Prints a message using both local and inherited value.
+    public double computeRatio()
+    {
+        return definitions/pages;
+    }
+
+    // Definitions mutator.
+    public void setDefinitions(int numDefinitions)
+    {
+        definitions = numDefinitions;
+    }
+
+    // Definitions accessor
+    public int getDefinitions()
+    {
+        return definitions;
+    }
+}
