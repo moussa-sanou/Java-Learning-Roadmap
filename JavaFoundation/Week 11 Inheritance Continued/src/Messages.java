da22880ac2f287cb6d39d0b5d45fa427deb9d93b
samidollars <muss.junior@gmail.com>
2025-02-15 22:38:47 -0600

    Week 11: Inheritance Demonstrates the use of an overridden method.

---
+++ JavaFoundation/Week 11 Inheritance Continued/src/Messages.java
@@ -0,0 +1,15 @@
+// Demonstrates the use of an overridden method.
+
+public class Messages {
+
+    // Creates two objects and invokes the message method in each.
+    public static void main(String[] args) {
+
+        Thought parked =  new Thought();
+        Advice dates = new Adivice();
+
+        parked.message();
+
+        dates.message();  // Overridden
+    }
+}
