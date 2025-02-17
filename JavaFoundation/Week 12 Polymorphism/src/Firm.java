93f7c4e95034f37a98dd2f55c1313b800092dce9
samidollars <muss.junior@gmail.com>
2025-02-16 22:17:14 -0600

    Week 12: Polymorphism Demonstrates polymorphism

---
+++ JavaFoundation/Week 12 Polymorphism/src/Firm.java
@@ -0,0 +1,12 @@
+// Demonstrates polymorphism via inheritance.
+
+public class Firm {
+
+    // Creates a staff of employee for a firm and pays them.
+    public static void main(String[] args) {
+
+        Staff personnel = new Staff();
+
+        personnel.payday();
+    }
+}
