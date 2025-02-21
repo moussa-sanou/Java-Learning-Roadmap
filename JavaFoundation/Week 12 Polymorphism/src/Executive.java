ca93a9876b76166ea06d41c9c91a1ce2e8930e2e
samidollars <muss.junior@gmail.com>
2025-02-20 22:27:50 -0600

    Week 12: Polymorphism Demonstrates polymorphism

---
+++ JavaFoundation/Week 12 Polymorphism/src/Executive.java
@@ -0,0 +1,32 @@
+// Represents an executive staff member, who can earn a bonus.
+
+public class Executive extends Employee {
+
+    private double bonus;
+
+    // Constructor: Sets up this executive with specified information.
+    public Executive(String eName, String eAddress, String ePhone,
+                     String socSecNumber, double rate)
+    {
+        super(eName, eAddress, ePhone, socSecNumber, rate);
+
+        bonus = 0;   // Bonus has yet to be awarded
+    }
+
+    // Awards the specified bonus to this executive.
+    public void awardBonus(double execBonus)
+    {
+        bonus = execBonus;
+    }
+
+    /* Computes and returns the pay for an executive, which is the regular
+    * employee payment plus one-time bonus. */
+    public double pay()
+    {
+        double payment = super.pay() + bonus;
+
+        bonus = 0;
+
+        return payment;
+    }
+}
---
+++ JavaFoundation/Week 12 Polymorphism/src/Hourly.java
@@ -0,0 +1,35 @@
+// Represents an employee that gets paid by the hour.
+
+public class Hourly extends Employee{
+
+    private int hoursWorked;
+
+    // Constructor: Sets up this hourly employee using specified information.
+    public Hourly(String eName, String eAddress, String ePhone,
+                  String socSecNumber, double rate)
+    {
+        super(eName, eAddress, ePhone, socSecNumber, rate);
+
+        hoursWorked = 0;
+    }
+
+    /* Adds the specified number of hours to this employee's accumulated hours */
+    public double pay()
+    {
+        double payment = payRate * hoursWorked;
+
+        hoursWorked = 0;
+
+        return payment;
+    }
+
+    // Returns information about this hourly employee as a string.
+    public String toString()
+    {
+        String result = super.toString();
+
+        result += "\nCurrent hours: " + hoursWorked;
+
+        return result;
+    }
+}
