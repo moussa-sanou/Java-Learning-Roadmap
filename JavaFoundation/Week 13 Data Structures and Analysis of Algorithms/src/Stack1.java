637eeda527fa70c75857d29c293ad107e451b835
samidollars <muss.junior@gmail.com>
2025-02-26 22:26:06 -0600

    Week 13: Data structures and algorithm analysis. List example

---
+++ JavaFoundation/Week 13 Data Structures and Analysis of Algorithms/src/Stack1.java
@@ -0,0 +1,30 @@
+// Java program Implementing stack class
+
+import java.util.Stack;
+
+public class Stack1 {
+
+    public static void main(String[] args) {
+
+        // Create a new stack
+        Stack<Integer> s = new Stack<>();
+
+        // Push elements from the stack
+        s.push(1);
+        s.push(2);
+        s.push(3);
+        s.push(4);
+
+        // Peek at the element on top of the stack
+        System.out.println(s.peek());
+
+        System.out.println("----------------");
+
+        // Pop elements from the stack
+        while (!s.isEmpty())
+        {
+            System.out.println(s.pop());
+        }
+
+    }
+}
